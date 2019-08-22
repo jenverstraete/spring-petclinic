77dc9df7085999db9209c042f072bfe0c6547330
Jen <jenverstraete@vanmarcke.be>
2019-08-22 16:45:20 +0200

    fixed failing test

--- pet-clinic-web/src/test/java/vivpetclinic/VivPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/vivpetclinic/VivPetClinicApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import viverit.vivpetclinic.VivPetClinicApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = VivPetClinicApplication.class)
 public class VivPetClinicApplicationTests {
 
     @Test
