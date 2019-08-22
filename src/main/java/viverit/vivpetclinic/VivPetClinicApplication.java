f4acc0c20960a5a0f60e1ed2c7649221bbc5c389
Jen <jenverstraete@vanmarcke.be>
2019-08-22 13:18:21 +0200

    Initial project add

---
+++ src/main/java/viverit/vivpetclinic/VivPetClinicApplication.java
@@ -0,0 +1,13 @@
+package viverit.vivpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VivPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/viverit/vivpetclinic/VivPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package viverit.vivpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VivPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
