6fed31ab09f4091d298e72a72ac16bf0bcb3ff5e
Jen <jenverstraete@vanmarcke.be>
2019-08-23 15:45:51 +0200

    closes #33

--- pet-clinic-data/src/main/java/viverit/vivpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/viverit/vivpetclinic/services/map/VetServiceMap.java
@@ -1,13 +1,21 @@
 package viverit.vivpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
+import viverit.vivpetclinic.model.Specialty;
 import viverit.vivpetclinic.model.Vet;
+import viverit.vivpetclinic.services.SpecialtyService;
 import viverit.vivpetclinic.services.VetService;
 
 import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(final SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -30,6 +38,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(final Vet owner) {
+        if (owner.getSpecialties().size() > 0) {
+            owner.getSpecialties().forEach(specialty ->
+            {
+                if (specialty.getId() == null) {
+                    Specialty savedSpeciality = specialtyService.save(specialty);
+                    specialty.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(owner);
     }
 }
--- pet-clinic-web/src/main/java/viverit/vivpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/viverit/vivpetclinic/bootstrap/DataLoader.java
@@ -2,12 +2,10 @@ package viverit.vivpetclinic.bootstrap;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import viverit.vivpetclinic.model.Owner;
-import viverit.vivpetclinic.model.Pet;
-import viverit.vivpetclinic.model.PetType;
-import viverit.vivpetclinic.model.Vet;
+import viverit.vivpetclinic.model.*;
 import viverit.vivpetclinic.services.OwnerService;
 import viverit.vivpetclinic.services.PetTypeService;
+import viverit.vivpetclinic.services.SpecialtyService;
 import viverit.vivpetclinic.services.VetService;
 
 import java.time.LocalDate;
@@ -17,16 +15,25 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialtyService specialtyService;
 
     public DataLoader(final OwnerService ownerService, final VetService vetService
-            , final PetTypeService petTypeService) {
+            , final PetTypeService petTypeService, final SpecialtyService specialtyService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialtyService = specialtyService;
     }
 
     @Override
     public void run(final String... args) {
+        int count = petTypeService.findAll().size();
+        if (count == 0) {
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -34,6 +41,16 @@ public class DataLoader implements CommandLineRunner {
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
+
+        Specialty radiology = new Specialty();
+        radiology.setDescription("radiology");
+        Specialty surgery = new Specialty();
+        surgery.setDescription("surgery");
+        Specialty dentistry = new Specialty();
+        dentistry.setDescription("dentistry");
+        Specialty savedRadiology = specialtyService.save(radiology);
+        Specialty savedSurgery = specialtyService.save(surgery);
+        Specialty savedDentistry = specialtyService.save(dentistry);
 
         Owner owner1 = new Owner();
         owner1.setId(1L);
@@ -69,12 +86,14 @@ public class DataLoader implements CommandLineRunner {
         vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialties().add(savedRadiology);
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+        vet2.getSpecialties().add(savedSurgery);
         vetService.save(vet2);
         System.out.println("Loaded Vets");
     }
