680f46526fe7914da22019de7cb7064986986a79
Jen <jenverstraete@vanmarcke.be>
2019-08-23 15:23:34 +0200

    closes #31

--- pet-clinic-web/src/main/java/viverit/vivpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/viverit/vivpetclinic/bootstrap/DataLoader.java
@@ -3,22 +3,35 @@ package viverit.vivpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import viverit.vivpetclinic.model.Owner;
+import viverit.vivpetclinic.model.PetType;
 import viverit.vivpetclinic.model.Vet;
 import viverit.vivpetclinic.services.OwnerService;
+import viverit.vivpetclinic.services.PetTypeService;
 import viverit.vivpetclinic.services.VetService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(final OwnerService ownerService, final VetService vetService) {
+    public DataLoader(final OwnerService ownerService, final VetService vetService
+            , final PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(final String... args) {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setId(1L);
         owner1.setFirstName("Michael");
