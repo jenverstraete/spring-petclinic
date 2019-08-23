d7ef6e0d9ef912da411c64d403f8d1766a370759
Jen <jenverstraete@vanmarcke.be>
2019-08-23 11:11:34 +0200

    closes #21

--- pet-clinic-web/src/main/java/viverit/vivpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/viverit/vivpetclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package viverit.vivpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import viverit.vivpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(final OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
