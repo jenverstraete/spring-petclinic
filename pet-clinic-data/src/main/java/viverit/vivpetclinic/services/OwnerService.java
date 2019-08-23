8d53190150c5e2ac703387e073ccc9d08c735864
Jen <jenverstraete@vanmarcke.be>
2019-08-23 09:21:13 +0200

    closes#10

--- pet-clinic-data/src/main/java/viverit/vivpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/viverit/vivpetclinic/services/OwnerService.java
@@ -2,14 +2,6 @@ package viverit.vivpetclinic.services;
 
 import viverit.vivpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/viverit/vivpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/viverit/vivpetclinic/services/PetService.java
@@ -2,12 +2,4 @@ package viverit.vivpetclinic.services;
 
 import viverit.vivpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
-}
+public interface PetService extends CrudService<Pet, Long> {}
