3f74f81014320b4279892fb9b47b01a94c2da6c5
Jen <jenverstraete@vanmarcke.be>
2019-08-26 11:00:06 +0200

    closes #35

--- pet-clinic-data/src/main/java/viverit/vivpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/viverit/vivpetclinic/model/Owner.java
@@ -1,12 +1,21 @@
 package viverit.vivpetclinic.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Owner extends Person{
+@Entity
+public class Owner extends Person {
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
