3be941a399a826dffdeb4dfd5a7d04caaa0309a9
Jen <jenverstraete@vanmarcke.be>
2019-08-26 11:06:28 +0200

    closes #37

--- pet-clinic-data/src/main/java/viverit/vivpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/viverit/vivpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package viverit.vivpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -19,6 +21,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -50,5 +55,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(final LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(final Set<Visit> visits) {
+        this.visits = visits;
     }
 }
