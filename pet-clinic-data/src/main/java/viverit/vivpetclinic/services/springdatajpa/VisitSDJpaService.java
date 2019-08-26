3a0ed0c0542f1b29d46d5558dc7acc0af4951e6b
Jen <jenverstraete@vanmarcke.be>
2019-08-26 11:43:27 +0200

    closes #46

---
+++ pet-clinic-data/src/main/java/viverit/vivpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,43 @@
+package viverit.vivpetclinic.services.springdatajpa;
+
+import viverit.vivpetclinic.model.Visit;
+import viverit.vivpetclinic.repositories.VisitRepository;
+import viverit.vivpetclinic.services.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VisitSDJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(final VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(final Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(final Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(final Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(final Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
