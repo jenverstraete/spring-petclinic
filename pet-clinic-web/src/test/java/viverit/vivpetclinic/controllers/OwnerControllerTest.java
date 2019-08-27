56387e03936313160bac0dc30cbd6b74c0616e66
Jen <jenverstraete@vanmarcke.be>
2019-08-27 11:46:06 +0200

    Closes #58

--- pet-clinic-web/src/test/java/viverit/vivpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/viverit/vivpetclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,8 @@ import viverit.vivpetclinic.services.OwnerService;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -63,5 +64,14 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("notimplemented"));
         verifyZeroInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
