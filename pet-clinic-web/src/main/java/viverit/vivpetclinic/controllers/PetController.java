fd678cf9f87c1ae53ee2504efccc68340a7089cb
Jen <jenverstraete@vanmarcke.be>
2019-08-27 14:20:41 +0200

    Closes #55

---
+++ pet-clinic-web/src/main/java/viverit/vivpetclinic/controllers/PetController.java
@@ -0,0 +1,46 @@
+package viverit.vivpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import viverit.vivpetclinic.model.Owner;
+import viverit.vivpetclinic.model.PetType;
+import viverit.vivpetclinic.services.OwnerService;
+import viverit.vivpetclinic.services.PetService;
+import viverit.vivpetclinic.services.PetTypeService;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("owners/{ownerId}")
+public class PetController {
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(final PetService petService, final OwnerService ownerService,
+                         final PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
