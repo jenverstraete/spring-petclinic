fb8ebc66cdfa30b494a542ce06a1be0c78305b7d
Jen <jenverstraete@vanmarcke.be>
2019-08-29 08:32:42 +0200

    Closes #66

--- pet-clinic-web/src/main/java/viverit/vivpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/viverit/vivpetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package viverit.vivpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import viverit.vivpetclinic.model.Vet;
 import viverit.vivpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -17,5 +22,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
